66c3b8113cf53dfd9bd08b6309c730f7e17a80de
mokendkomer <me@mukundkumar.com>
2020-08-24 11:48:25 +0530

    Update afk.js

--- afk.js
+++ afk.js
@@ -1,4 +1,4 @@
-const config = require('./config.json')
+const config = require('../config.json')
 const fs = require('fs');
 const Discord = require("discord.js");
 const client = new Discord.Client();
