dc77adbe2894b02e9b2ea4d02e64036494bff5e1
mokendkomer <me@mukundkumar.com>
2020-08-24 11:48:52 +0530

    Update alishatalk.js

--- alishatalk.js
+++ alishatalk.js
@@ -1,4 +1,4 @@
-const config = require('./config.json')
+const config = require('../config.json')
 const cleverbot = require("cleverbot-free");
 const Discord = require("discord.js");
 const client = new Discord.Client();
