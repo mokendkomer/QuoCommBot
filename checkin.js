f1878983ddcb9b1974620a9ad322935885d9d691
mokendkomer <me@mukundkumar.com>
2020-09-01 23:39:32 +0530

    became a smort goiiiii

--- checkin.js
+++ checkin.js
@@ -1,7 +1,23 @@
 const config = require('../config.json');
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
 const channel = "749244006705397761";
+const doesMatter = (reaction) => {
+    if(
+            reaction.emoji.name === "❤️" ||
+            reaction.emoji.name === "🧡" ||
+            reaction.emoji.name === "💛" ||
+            reaction.emoji.name === "🤍" ||
+            reaction.emoji.name === "🖤" ||
+            reaction.emoji.name === "💚" ||
+            reaction.emoji.name === "💙" ||
+            reaction.emoji.name === "💜" ||
+            reaction.emoji.name === "💔"
+    )
+            return true;
+        else
+            return false;
+}
 
 client.on('message', message => {
     if(message.channel.id === channel && message.content.toLowerCase().includes("💙")){
@@ -18,6 +34,24 @@ client.on('message', message => {
         } catch(err){console.log(err)}
     }
 });
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+    }
+    
+    if(reaction.message.channel.id === channel && !doesMatter(reaction))
+        reaction.remove();
+
+})
 
 client.on('error', err => console.log(err))
 
