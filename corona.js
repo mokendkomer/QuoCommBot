c0d39d94ce7d5e064475e685fa2b71be6a3791a9
mokendkomer <me@mukundkumar.com>
2020-09-21 19:13:39 +0530

    Update corona.js
    added commas, cleaned it up

--- corona.js
+++ corona.js
@@ -2,97 +2,48 @@ const config = require('../config.json')
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const axios = require("axios").default;
-
-const sendMessage = (msg, response, thumbnail) => {
-      const exampleEmbed = {
-        color: "0x" + msg.member.roles.highest.hexColor,
-        title: "COVID19 Stats for " + response.data.country,
-        author: {
-          name: msg.member.nickname,
-          icon_url: msg.author.avatarURL(),
-        },
-        thumbnail: {
-          url: thumbnail,
-        },
-        fields: [
-          {
-            name: "Cases",
-            value: response.data.cases,
-            inline: true,
-          },
-          {
-            name: "Active",
-            value: response.data.active,
-            inline: true,
-          },
-          {
-            name: "Recovered",
-            value: response.data.recovered,
-            inline: true,
-          },
-          {
-            name: "Critical",
-            value: response.data.critical,
-            inline: true,
-          },
-          {
-            name: "Deaths",
-            value: response.data.deaths,
-            inline: true,
-          },
-          {
-            name: "Cases Today",
-            value: response.data.todayCases,
-            inline: true,
-          },
-          {
-            name: "Cases Per One Million",
-            value: response.data.casesPerOneMillion,
-              inline: true,
-            },
-            {
-              name: "Deaths  Per One Million",
-              value: response.data.deathsPerOneMillion,
-              inline: true,
-            },
-            {
-            name: "Deaths Today",
-            value: response.data.todayDeaths,
-            inline: true,
-            },
-        ],
-        footer: {
-            text: "Tests: " + response.data.tests,
-            icon_url: msg.guild.iconURL(),
-        },
-      };
-      msg.channel.send({ embed: exampleEmbed });
+const sendMessage = (message, response) => {
+  const embed = new Discord.MessageEmbed();
+  embed.setColor("0x" + message.member.roles.highest.hexColor)
+  if(response.data.country){
+    embed.setTitle("COVID-19 Stats for " + response.data.country);
+    embed.setThumbnail(response.data.countryInfo.flag);
+  }
+  else{
+    embed.setTitle("WorldWide COVID-19 Stats");
+    embed.setThumbnail("https://icons.iconarchive.com/icons/dtafalonso/modern-xp/512/ModernXP-73-Globe-icon.png");
+  }
+  embed.setAuthor(message.member.nickname, message.author.avatarURL());
+  embed.addField("Cases", response.data.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Active", response.data.active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Recovered", response.data.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Critical", response.data.critical.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Deaths", response.data.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Cases Today", response.data.todayCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Cases Per One Million", response.data.casesPerOneMillion.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Deaths  Per One Million", response.data.deathsPerOneMillion.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.addField("Deaths Today", response.data.todayDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), true);
+  embed.setFooter(`Tests: ${response.data.tests}`, message.guild.iconURL());
+  message.channel.send({ embed });
 }
-
 client.on("ready", () => {
   console.log("ez");
 });
-
-client.on("message", (msg) => {
+client.on("message", (message) => {
   if (
-    (msg.channel.id === "587152950078734348" ||
-      msg.channel.id === "692489104268591144") &&
-    msg.content.toLowerCase().startsWith("q.corona")
+    (message.channel.id === "587152950078734348" ||
+      message.channel.id === "692489104268591144") &&
+    message.content.toLowerCase().startsWith("q.corona")
   ) {
-    let country = msg.content.toLowerCase().substr(9);
-    if (country === "") {
-      axios
-        .get("http://corona.lmao.ninja/v2/all")
-        .then((response) => sendMessage(msg, response, "https://icons.iconarchive.com/icons/dtafalonso/modern-xp/512/ModernXP-73-Globe-icon.png"))
-        .catch(function (error) {
-          msg.channel.send("Something went wrong");
-        });
-    } else {
-        axios
-    .get("http://corona.lmao.ninja/v2/countries/" + country)
-    .then((response) => sendMessage(msg, response, response.data.countryInfo.flag))
-    .catch((error) => msg.channel.send("Something went wrong"))
-    }
+    let country = message.content.toLowerCase().substr(9);
+    if (!country.length)
+      country = "all"
+    else 
+      country = `countries/${country}`
+    axios
+    .get("http://corona.lmao.ninja/v2/" + country)
+    .then((response) => sendMessage(message, response))
+    .catch((error) => message.channel.send("Something went wrong"))
   }
 });
 
