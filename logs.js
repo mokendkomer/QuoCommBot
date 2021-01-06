b9254a71ade4aa962aa2eb1c03ea7bdd52455317
mokendkomer <me@mukundkumar.com>
2021-01-06 20:29:26 +0530

    added mutes, selfmutes, and big mokend magic

--- logs.js
+++ logs.js
@@ -27,7 +27,15 @@ const getInviteCounts = async guild => {
 client.on('ready', async () => {
     console.log('ready')
     invites['587139618999369739'] = await getInviteCounts(client.guilds.cache.get('587139618999369739'));
-    connection.query(`select * from mutes`)
+    client.setInterval(() => {
+		connection.query(`select id from mutes where current_timestamp() > time`, function (error, result) {
+			result.forEach(ele => {
+				const muted = client.guilds.cache.get('587139618999369739').members.cache.get(ele.id)
+				muted.roles.remove('751530631489650738')
+			})
+		});
+	}, 60000);
+
 });
 client.on('messageReactionAdd', (reaction, user) => {
     if(reaction.message.guild.id === "587139618999369739"){
@@ -58,8 +66,6 @@ client.on('guildMemberAdd', async (member) => {
     }
 
     connection.query(`select verified, not isnull(mu.id) as muted from members m left join mutes mu using (id) where id = ?`, [member.id], function (error, result) {
-        if(error) console.log(error)
-        console.log(result.length)
         if (!result.length)
             connection.query(`INSERT INTO members (id, name) VALUES (?, ?)`, [member.id, member.displayName])
         else {
@@ -68,16 +74,90 @@ client.on('guildMemberAdd', async (member) => {
             }
         }
     });
+})
+client.on('message', message => {
+	if (message.content.toLowerCase().startsWith('q.mute')) {
+		let args = message.content.toLowerCase().split(" ").slice(1);
+		let toMute = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+		if (!message.member.hasPermission("MANAGE_MESSAGES"))
+			return message.channel.send("You do not have the permission to mute someone.");
+		if(toMute.id === message.member.id)
+			return message.channel.send(`Go fuck yourself.`)
+		if (!toMute)
+			return message.channel.send("You did not mention a user or ID.");
+		let mutedRole = message.guild.roles.cache.get('751530631489650738')
+		if (toMute.roles.cache.has(mutedRole.id))
+			return message.channel.send("This user is already muted.");
 
+		if (!args[1] && !args[2]) {
+			toMute.roles.add(mutedRole)
+			message.channel.send(`${toMute.displayName} has been muted.`)
+		}
+		if (args[1] && args[2]) {
+			let time = parseInt(args[1])
+			if (time === NaN)
+				return message.channel.send(`${args[1]} is not a number.`);
+			if (args[2].startsWith('m'))
+				connection.query(`INSERT INTO mutes VALUES (?, CURRENT_TIMESTAMP() + INTERVAL ? MINUTE)`, [toMute.id, time])
+			else if (args[2].startsWith('h'))
+				connection.query(`INSERT INTO mutes VALUES (?, CURRENT_TIMESTAMP() + INTERVAL ? HOUR)`, [toMute.id, time])
+			else if (args[2].startsWith('d'))
+				connection.query(`INSERT INTO mutes VALUES (?, CURRENT_TIMESTAMP() + INTERVAL ? DAY)`, [toMute.id, time])
+			else return message.channel.send(`${args[2]} is not a unit of time.`);
+			toMute.roles.add(mutedRole)
+			message.channel.send(`${toMute.displayName} has been muted.`)
+		}
+	} else 	if (message.content.toLowerCase().startsWith('q.selfmute')) {
+		let args = message.content.toLowerCase().split(" ").slice(1);
+		let toMute = message.member;
+		let mutedRole = message.guild.roles.cache.get('751530631489650738')
+
+		if (!args[0] && !args[1]) {
+			message.channel.send(`Please specify the duration of your mute in minutes, hours, or days.`)
+		}
+		if (args[0] && args[1]) {
+			let time = parseInt(args[0])
+			if (time === NaN)
+				return message.channel.send(`${args[0]} is not a number.`);
+			if (args[1].startsWith('m'))
+				connection.query(`INSERT INTO mutes VALUES (?, CURRENT_TIMESTAMP() + INTERVAL ? MINUTE)`, [toMute.id, time])
+			else if (args[1].startsWith('h'))
+				connection.query(`INSERT INTO mutes VALUES (?, CURRENT_TIMESTAMP() + INTERVAL ? HOUR)`, [toMute.id, time])
+			else if (args[1].startsWith('d'))
+				connection.query(`INSERT INTO mutes VALUES (?, CURRENT_TIMESTAMP() + INTERVAL ? DAY)`, [toMute.id, time])
+			else return message.channel.send(`${args[1]} is not a unit of time.`);
+			toMute.roles.add(mutedRole)
+			message.channel.send(`You have been muted.`)
+		}
+	} else if (message.content.toLowerCase().startsWith('q.unmute')) {
+		let args = message.content.toLowerCase().split(" ").slice(1);
+		if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have Permission to unmute.");
+		let toUnmute = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+		if (!toUnmute) return message.channel.send("You did not specify a user mention or ID.");
+		let mutedRole = message.guild.roles.cache.get('751530631489650738')
+		if (!toUnmute.roles.cache.has(mutedRole.id)) return message.channel.send("This user is not muted.");
+
+		toUnmute.roles.remove(mutedRole)
+		message.channel.send(`${toUnmute.displayName} has been unmuted.`)
+	}
 })
 
 client.on('guildMemberUpdate', (oldMember, newMember) => {
     if (oldMember.displayName !== newMember.displayName) {
         connection.query(`UPDATE members SET name = ? WHERE id = ?`, [newMember.displayName, newMember.id])
-    }
-    if (!oldMember.roles.cache.get('587187354851082250') && newMember.roles.cache.get('587187354851082250')) {
+    } else if (!oldMember.roles.cache.get('587187354851082250') && newMember.roles.cache.get('587187354851082250')) {
         connection.query(`UPDATE members SET verified = 1 WHERE id = ?`, [newMember.id])
-    }
+    } else if (!oldMember.roles.cache.get('751530631489650738') && newMember.roles.cache.get('751530631489650738')) {
+		newMember.send(`You have been muted.`)
+		connection.query(`INSERT INTO mutes VALUES (?, NULL)`, [newMember.id], (err) => {
+			if(err) console.log('')
+		})
+	} else if (oldMember.roles.cache.get('751530631489650738') && !newMember.roles.cache.get('751530631489650738')) {
+		newMember.send(`You have been unmuted.`)
+		connection.query(`DELETE FROM mutes WHERE id = ?`, [newMember.id], (err) => {
+			if(err) console.log('')
+		})
+	}
 })
 
 
