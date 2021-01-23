a8dd44aaf57ad7e20f066956ec3307982ba6a97e
mokendkomer <me@mukundkumar.com>
2021-01-23 18:21:05 +0530

    Update qa.js

--- qa.js
+++ qa.js
@@ -9,11 +9,26 @@ client.on("ready", () => {
 client.on("message", (message) => {
     if(message.channel.type === "dm"){
         if(message.content.toLowerCase().startsWith('q.ask')){
-            client.guilds.cache.get('587139618999369739').channels.cache.get('587321519051636776').send("<@" + message.author.id + "> asks " + message.content.substring(6));
-            client.guilds.cache.get('587139618999369739').channels.cache.get('639902815849938975').send(message.content.substring(6));
+            client.channels.cache.get('587219379456966701').send(`${client.guilds.cache.get('587139618999369739').member(message.author.id).displayName} asks ${message.content.substring(6)}`);
+            client.channels.cache.get('639902815849938975').send(message.content.substring(6));
             message.channel.send(`Thank you for your question!`);
+        }
+        else if(message.content.toLowerCase().startsWith('q.vent1')){
+            client.channels.cache.get('587219379456966701').send(`${client.guilds.cache.get('587139618999369739').member(message.author.id).displayName} vents ${message.content.substring(8)} in vent 1`);
+            client.channels.cache.get('739150769722228806').send(message.content.substring(8));
+            message.channel.send(`Your message has been sent. Take care!`);
+        }
+        else if(message.content.toLowerCase().startsWith('q.vent ')){
+            client.channels.cache.get('587219379456966701').send(`${client.guilds.cache.get('587139618999369739').member(message.author.id).displayName} vents ${message.content.substring(7)} in vent 1`);
+            client.channels.cache.get('739150769722228806').send(message.content.substring(7));
+            message.channel.send(`Your message has been sent. Take care!`);
+        }
+        else if(message.content.toLowerCase().startsWith('q.vent2')){
+            client.channels.cache.get('587219379456966701').send(`${client.guilds.cache.get('587139618999369739').member(message.author.id).displayName} vents ${message.content.substring(8)} in vent 2`);
+            client.channels.cache.get('793407631066005554').send(message.content.substring(8));
+            message.channel.send(`Your message has been sent. Take care!`);
         }
     }
 });
 
-client.login(config.token);
+client.login(config.anonToken);
